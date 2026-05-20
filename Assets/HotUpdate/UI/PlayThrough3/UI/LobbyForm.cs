178455f6bc8412ae6b1613203ca57f833445d9d0
butterfly301 <3518786056@qq.com>
2026-05-20 23:28:20 +0800

    提交

--- Assets/HotUpdate/UI/PlayThrough3/UI/LobbyForm.cs
+++ Assets/HotUpdate/UI/PlayThrough3/UI/LobbyForm.cs
@@ -7,13 +7,36 @@ using UnityEngine.UI;
 public class LobbyForm : MonoBehaviour,IAutoBind
 {
     [SerializeField]private Button btnFriend;
+    [SerializeField]private Button btnSignIn;
     public void Init()
     {
         btnFriend.onClick.AddListener(OnBtnFriendClick);
+        btnSignIn.onClick.AddListener(OnBtnSignInClick);
     }
 
     private void OnBtnFriendClick()
     {
         UIManager3.Instance.Tip?.Show("好友系统正在开发中，敬请期待！");
+    }
+    private void OnBtnSignInClick()
+    {
+        ConfirmWindowData data = new ConfirmWindowData
+        {
+            title = "签到",
+            content = "是否要进行签到？",
+            confirmData = new confirmData
+            {
+                confirmButtonText = "签到",
+                onConfirm = () =>
+                {
+                    UIManager3.Instance.Tip?.Show("签到成功！");
+                }
+            },
+            cancelData = new cancelData
+            {
+                cancelButtonText = "算了"
+            }
+        };
+        UIManager3.Instance.Confirm?.Open(data);
     }
 }
